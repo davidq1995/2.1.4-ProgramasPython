805afcbcffccdfae9a4dc3a1da8904352cc8ef06
davidq1995 <davidparada95@gmail.com>
2024-04-16 21:03:02 -0400

    CLASE MARTES

---
+++ ProgramasPython/fafdfdw.py
@@ -0,0 +1,69 @@
+#Edad= int(input("Ingresa tu edad"));
+#variable_entera= int(input("Ingresa un numero entero wey:"));
+#variable_decimal=float(input("Ingresa un decimal");)
+#variable_texto= str(input("Ingresa una cadena de texto"));
+#variable_booleana= bool(input("Ingresa un valor booleanoo"));
+
+
+#print(Edad);
+#print(variable_entera);
+#print(variable_decimal);
+#print(variable_texto);
+#print(variable_booleana);
+
+
+#SIGUIENTE (PROMEDIO)
+
+#for i in range(1,20):
+#    print(i)
+
+#SIGUIENTE
+#num=0
+#for i in range(3):
+#    print("ingrese un numero")
+#    num=num+int(input())
+#print(num/3)
+
+
+#SIGUIENTE
+
+#Multiplicacion hasta 10
+#factoromulti=int(input("Ingrese el numero del cual desea conocer la tabla hasta el 10: 8"));
+#for i in range (1,11):
+#    print(factoromulti,"x",i, "=", factoromulti* i)
+#(CUANDO DEJO UN ESPACIO ES PORQUE NECESITO QUE VAYA CON TABULADOR)
+
+#SIGUIENTE
+
+#number=7
+#edad=64
+#if number<edad:
+#    print("La variable booleana es verdadera.")
+#elif edad > 50:
+#    print("La suma es mayor que 50.")
+#else:
+#    print("Ninguna de las condiciones anteriores se cumplen")
+
+
+#Alex=15
+#Sam=24
+#Ada=60
+#if Alex<Sam:
+#    if Sam>Ada:
+#       print("Sam es el mayor de todos.")
+#    else:
+#        print("Sam es solo mayor que Alex")
+
+
+#SIGUIENTE
+
+# num1=1
+# num2=2
+# num3=3
+
+# if num1>num2 and num1>num3:
+#     print ("El numero mayor es", num1)
+#     if num2>num3:
+#        print("El numero mayor es", num2) 
+#     else:
+#         print("El numero mayor es", num3)
